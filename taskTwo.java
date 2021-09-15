9dd8e56a66271cb8c55196cdf40b163ad1667f8b
EvgeniyZharov <76780101+EvgeniyZharov@users.noreply.github.com>
2021-09-15 10:15:35 +0300

    Add files via upload

---
+++ taskTwo.java
@@ -0,0 +1,10 @@
+package com.company;
+
+class MainTwo{
+    public static void main(String[] args){
+        for (String name : args){
+            System.out.println(name);
+        }
+        System.out.println("Hello, Big World!");
+    }
+}
