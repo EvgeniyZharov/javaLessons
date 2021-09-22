ab76aca36ae8d3d5784987846ee20c09fc13107c
EvgeniyZharov <76780101+EvgeniyZharov@users.noreply.github.com>
2021-09-22 14:50:57 +0300

    Delete taskThree.java

--- taskThree.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-class TaskThree {
-    public static void main(String[] args){
-        for (float i = 1; i < 11; i++){
-            System.out.println(1 / i);
-        }
-    }
-}
