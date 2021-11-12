a56e1b48f21a914221afdd4c54b9bc208d1a86fa
EvgeniyZharov <76780101+EvgeniyZharov@users.noreply.github.com>
2021-11-12 13:01:34 +0300

    Add files via upload

--- Excercise_2/task_1.java
+++ Excercise_2/task_1.java
@@ -10,7 +10,13 @@ class Task_1{
         Book book = new Book();
 
         Dog dog = new Dog(5, "Бобик");
-        System.out.println(dog.toStrign());
+        System.out.println(dog.toString() + "\n");
+
+        TestDog dogs = new TestDog(3);
+        dogs.newDog(3, "Чижик");
+        dogs.newDog(3, "Жучка");
+        dogs.newDog(4, "Каштанка");
+        dogs.getInfo();
     }
 }
 
@@ -48,10 +54,33 @@ class Dog{
 
     public int getHommeAge(){ return this.age * 7; }
 
-    public String toStrign(){
+    public String toString(){
         String line = "Это собака по кличке: " + getName() + "\n";
         line += "Ее возраст: " + getAge() + " лет\n" + "По человеческим меркам: " + getHommeAge() + "лет";
         return line;
     }
 
+}
+
+class TestDog{
+    private Dog[] dogs;
+    private int existDog = 0;
+    private int countDog;
+
+    TestDog(int countDogs){ this.dogs = new Dog[countDogs]; this.countDog = countDogs; }
+
+    public void newDog(int age, String name){
+        this.dogs[existDog] = new Dog(age, name);
+        this.existDog++;
+    }
+
+    public void getInfo(){
+        if (existDog == countDog) for (Dog dog : this.dogs) System.out.println(dog.toString() + "\n");
+        else {
+            Dog[] new_dogs = new Dog[existDog];
+            System.arraycopy(this.dogs, 0, new_dogs, 0, existDog);
+            for (Dog dog : new_dogs) System.out.println(dog.toString() + "\n");
+
+        }
+    }
 }
